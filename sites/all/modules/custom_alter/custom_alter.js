413ebc314a65d3dad333b7b5e8f82b5da064c274
pradip-theanuswara <pradip@theanuswara.com>
2015-01-25 18:34:42 +0530

    Update custom_alter.js

--- sites/all/modules/custom_alter/custom_alter.js
+++ sites/all/modules/custom_alter/custom_alter.js
@@ -76,6 +76,9 @@ $('#belief_container').scroll(function() {
 buffer = 30 // # of pixels from bottom of scroll to fire your function. Can be 0
 if (belief_container.offsetHeight + belief_container.scrollTop >= belief_container.scrollHeight) {
 $('#edit-field-beliveaccept-und').attr("disabled", false);
+
+
+$('#edit-field-beliveaccept-und').text().replace('Belive','Vijay'); // Vijay 
   }
 });
 
@@ -96,7 +99,7 @@ $('#edit-field-accept-und').attr("disabled", false);
   }
 });
 
-$('#edit-field-accept-und').attr("disabled", true);
+//$('#edit-field-accept-und').attr("disabled", true);
 
 if($('#edit-field-accept-und').attr('checked')) {
 $('#edit-field-accept-und').attr("disabled", false);
@@ -338,5 +341,4 @@ $("#edit-field-what-is-your-role-with-thi-und-0-value").attr('maxlength','50');
 	});
 
 	});
-        
 })(jQuery);
